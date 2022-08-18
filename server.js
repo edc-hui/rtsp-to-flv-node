ad5bea199e456cbd80cbea4e067a5d08d446b2fa
惠思雨 <huisiyu@bimwinner.com>
2022-08-18 13:46:05 +0800

    更改ffmpeg配置

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ const expressWebSocket = require('express-ws');
 
 ffmpeg.setFfmpegPath(ffmpegPath.path);
 
-console.log('ffmpeg 的路径', ffmpegPath.path);
-console.log('ffmpeg 的版本', ffmpegPath.version);
-
 /**
  * 创建一个后端服务
  */
 function createServer() {
     const app = express();
     app.use(express.static(__dirname));
-    // const server = http.createServer(app);
     expressWebSocket(app, null, {
         perMessageDeflate: true
     });
@@ -30,6 +26,11 @@ function createServer() {
     });
 }
 
+/**
+ * rtsp 转换 flv 的处理函数
+ * @param ws
+ * @param req
+ */
 function rtspToFlvHandle(ws, req) {
     const stream = webSocketStream(ws, {
         binary: true,
@@ -47,6 +48,7 @@ function rtspToFlvHandle(ws, req) {
                 '-buffer_size', '102400'
             )
             .on('start', (commandLine) => {
+                // commandLine 是完整的ffmpeg命令
                 console.log(commandLine, '转码 开始');
             })
             .on('codecData', function (data) {
@@ -64,14 +66,14 @@ function rtspToFlvHandle(ws, req) {
                 console.log(url, '转码 结束!');
             })
             .addOutputOption(
-                '-threads', '4',
+                '-threads', '4',  // 一些降低延迟的配置参数
                 '-tune', 'zerolatency',
-                '-preset', 'ultrafast'
+                '-preset', 'superfast'
             )
-            .outputFormat('flv')
-            .videoCodec('libx264')
-            .withSize('320x240')
-            .noAudio()
+            .outputFormat('flv') // 转换为flv格式
+            .videoCodec('libx264') // ffmpeg无法直接将h265转换为flv的，故需要先将h265转换为h264，然后再转换为flv
+            // .withSize('50%') // 转换之后的视频分辨率原来的50%, 如果转换出来的视频仍然延迟高，可按照文档上面的描述，自行降低分辨率
+            .noAudio() // 去除声音
             .pipe(stream);
     } catch (error) {
         console.log('抛出异常', error);
